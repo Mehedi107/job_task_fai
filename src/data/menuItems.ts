fa4a6c4110225de1be0cec18ada0314e0d86ebfe
Mehedi Hasan <mehedi.hsn.dev.@gmail.com>
2026-02-02 19:56:03 +0600

    fix call list menu title

--- src/data/menuItems.ts
+++ src/data/menuItems.ts
@@ -6,7 +6,7 @@ import settingsIcon from "@/assets/settings.png"
 // Your menu data
 export const menuItems = [
   { id: "overview", label: "Dashboard Overview", icon: homeIcon, },
-  { id: "logs", label: "Call Logs", icon: phoneIcon },
+  { id: "logs", label: "Call Logs & History", icon: phoneIcon },
   { id: "appointments", label: "Appointments", icon: calenderIcon },
   { id: "settings", label: "Settings", icon: settingsIcon },
 ]
